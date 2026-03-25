0501c2f93d220d681ac8899c0bac593905c1e29b
camilonietoma <102198208+camilonietoma@users.noreply.github.com>
2026-03-25 20:40:38 +0000

    Deploying to gh-pages from @ camilonietoma/camilonietoma@c07f2bc3d2b310fe892eaa83bc7d7bd54d54162a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "publications",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{id: "dropdown-publications",
+  },{id: "dropdown-publications",
               title: "publications",
               description: "",
               section: "Dropdown",
